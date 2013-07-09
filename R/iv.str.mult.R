faab3ad93bf3749a785619e21ff33f9ccabe966b
Tomáš Greif <tgr.cpnet>
2013-07-09 14:56:19 +0200

    Replace iv.str.all with iv.str.mult. Now it is possible to send custom vector of vars to function.

---
+++ R/iv.str.mult.R
@@ -0,0 +1,33 @@
+#' Calculate Information Value for all character columns in data frame
+#'
+#' Calculates information value for all character vectors in given data frame
+#'
+#' @param df data frame with at least two columns
+#' @param y column (integer) with binary outcome
+#' @param summary Only total information value for variable is returned when summary is TRUE
+#' @param vars List of variables. If not specified, all character variables will be used
+#' @export
+#' @examples
+#' iv.str.mult(german_data,"gbbin")
+#' iv.str.mult(german_data,"gbbin",T)
+#' iv.str.mult(german_data,"gbbin",T,c("ca_status","housing","job"))
+#' iv.str.mult(german_data,"gbbin",vars=c("ca_status","housing","job"))
+
+iv.str.mult <- function(df,y,summary=F,vars=NULL) {
+  if(is.null(vars)) {
+    char_col_bol <- sapply(names(df),function (x) is.character(df[,x]))
+    char_col_list <- c(names(char_col_bol[which(char_col_bol==TRUE)]))    
+  } else {
+    char_col_list = vars    
+  }
+
+  ivlist <- lapply(char_col_list, function (x) iv.str(df,x,y))
+  
+  if (!summary) {
+    ivlist <- lapply(char_col_list, function (x) iv.str(df,x,y))
+  } else {
+    ivlist <- rbind.fill(ivlist)
+    ivlist <- aggregate(ivlist$miv, by=list(Category=ivlist$variable), FUN=sum)    
+  }
+  ivlist
+}
