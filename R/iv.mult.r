af26aaf2c545f5ea3860048a3d40ce089495cf5d
Tomáš Greif <tgr.cpnet>
2013-07-12 14:28:31 +0200

    V.2 Numeric variables can be processed using rpart.

---
+++ R/iv.mult.r
@@ -0,0 +1,54 @@
+#' Calculate Information Value for defined columns in data frame
+#'
+#' Calculates information value for defined columns in given data frame. Columns can have numeric or character type.
+#'
+#' @param df data frame with at least two columns
+#' @param y column (integer) with binary outcome
+#' @param summary Only total information value for variable is returned when summary is TRUE. Output is sorted by
+#' information value, starting with highest value.
+#' @param vars List of variables. If not specified, all character variables will be used
+#' @export
+#' @examples
+#' iv.mult(german_data,"gbbin")
+#' iv.mult(german_data,"gbbin",T)
+#' iv.mult(german_data,"gbbin",T,c("ca_status","housing","job","mob")) # str(german_data)
+#' iv.mult(german_data,"gbbin",vars=c("ca_status","housing","job","mob","gb"))
+#' iv.mult(german_data,vars=c('ca_status','mob','credit_history','purpose',
+#'                            'credit_amount','savings','present_employment_since',
+#'                            'status_sex','installment_rate_income','other_debtors',
+#'                            'present_residence_since','property','age','other_installment',
+#'                            'housing','existing_credits','job','liable_maintenance_people',
+#'                            'telephone','foreign_worker'),
+#'                            "gbbin", summary=T)
+
+
+iv.mult <- function(df,y,summary=F,vars=NULL) {
+
+  if(is.null(vars)) {
+    vars <- names(df)[names(df) !=y]
+  }
+  
+  ivlist <- lapply(vars, function (x) {
+      if(is.numeric(df[,x])) {
+        iv.num(df,x,y)
+      } else iv.str(df,x,y)
+    }
+                   )
+  
+  if (summary) {
+    ivlist <- rbind.fill(ivlist)
+    ivlist <- aggregate(ivlist$miv, by=list(Category=ivlist$variable), FUN=sum)    
+    ivlist <- ivlist[with(ivlist,order(-x)), ]
+    
+    ivlist$Strength[ivlist$x >= 1] <- 1
+    ivlist$Strength[ivlist$x > .5 & ivlist$x < 1] <- 2
+    ivlist$Strength[ivlist$x > .2 & ivlist$x < .5] <- 3
+    ivlist$Strength[ivlist$x > .1 & ivlist$x < .2] <- 4
+    ivlist$Strength[ivlist$x > .02 & ivlist$x < .1] <- 5
+    ivlist$Strength[ivlist$x <= .02] <- 6
+    ivlist$Strength <- as.factor(ivlist$Strength)
+    #levels(ivlist$Strength) <- c("Suspicious","Very strong","Strong","Average","Weak")
+    names(ivlist) <- c("Variable","InformationValue","Strength")
+  }
+  ivlist
+}
--- R/iv.num.r
+++ R/iv.num.r
@@ -11,9 +11,27 @@
 
 iv.num <- function(df,x,y) {
 
-  model   <- rpart(data=df,formula=df[,y]~df[,x],control=rpart.control(cp=0.001,minbucket=nrow(df)/100))
-  df["___tmp_class_iv___"] <- as.character(model$where)
-  iv_data <- iv.str(df,"___tmp_class_iv___",y)
-  iv_data["variable"] <- x
-  iv_data
+#df=german_data
+#x="mob"
+#y="gbbin"
+
+  model   <- rpart(data=df,formula=df[,y]~df[,x],control=rpart.control(cp=0.001,minbucket=nrow(df)/10))
+  model_where <- data.frame(node_id=model$where,obs_id=as.numeric(names(model$where)),stringsAsFactors=F) # str(model_where)
+  model_frame <- data.frame(model$frame,tree_node=rownames(model$frame),node_id=row(model$frame["var"]))
+  
+  log <- capture.output({
+    rpart.rules <- path.rpart(model,rownames(model$frame)[model$frame$var=="<leaf>"])
+  })  
+  tree_rules <- iv.parse.rpart.rule(x,rpart.rules)
+    
+  t <- sqldf("select obs_id, tr.class_label as tmp_iv_calc_label
+              from 
+                  model_where mw
+                  join model_frame mf using (node_id)        
+                  join tree_rules tr using (tree_node)")
+  df <- merge(df, t["tmp_iv_calc_label"], by=0, all=TRUE) # str(df)
+  iv_data <- iv.str(df,"tmp_iv_calc_label",y)
+  iv_data$variable <- x
+  sqldf("select iv.* from iv_data iv join tree_rules tr on (iv.class=tr.class_label) order by tr.min")
 }
+
