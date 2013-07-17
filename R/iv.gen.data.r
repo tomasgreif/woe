8bc96f3208dc7092ed537554bf1c1ac218a70fda
Tomáš Greif <tgr.cpnet>
2013-07-17 14:23:59 +0200

    Changed to on-demand df generation.

--- R/iv.gen.data.r
+++ R/iv.gen.data.r
@@ -1,18 +1,14 @@
-#' Recreate sample datasets
+#' Create bigger dataset by replicating german data dataset
 #'
-#' Internal function used to simulate bigger datasets
+#' Create bigger dataset by replicating german data dataset N times
 #'
+#' @param reps Number of replications
+#' @export
+#' @examples
+#' german_data_big <- iv.gen.data(reps=1)
 
-iv.gen.data <- function() {
-  german_data_10k <- german_data[rep(1:nrow(german_data),10),]
+iv.gen.data <- function(reps=1) {
+  german_data_big <- german_data[rep(1:nrow(german_data),reps),]
   rownames(german_data) <- seq_along(german_data[,1])
-  save(german_data_10k,file="data//german_data_10k.RData")
-  
-  german_data_100k <- german_data_10k[rep(1:nrow(german_data_10k),10),]
-  rownames(german_data_100k) <- seq_along(german_data_100k[,1])
-  save(german_data_100k,file="data//german_data_100k.RData")
-  
-  german_data_1000k <- german_data_100k[rep(1:nrow(german_data_100k),10),]
-  rownames(german_data_1000k) <- seq_along(german_data_1000k[,1])
-  save(german_data_1000k,file="data//german_data_1000k.RData")  
+  german_data_big
 }
