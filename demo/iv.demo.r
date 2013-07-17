4c4564edbaa1f370f4015ead9179d230b09cb8fa
Tomáš Greif <tgr.cpnet>
2013-07-17 16:21:26 +0200

    Demo

---
+++ demo/iv.demo.r
@@ -0,0 +1,19 @@
+#' riv demo
+#'
+#' Ilustrate basic usage
+#'
+#' @export
+
+# Get summary IV for data frame
+iv.mult(german_data,"gbbin",summary=TRUE)
+
+# Plot summary IV for data frame
+iv.plot.summary(iv.mult(german_data,"gbbin",summary=TRUE))
+
+# Plot WoE patterns for individual variables
+iv.plot.woe(iv=iv.mult(german_data,"gbbin",vars=c("age","mob","ca_status")))
+
+# Plot WoE patterns for all variables
+iv.plot.woe(iv=iv.mult(german_data,"gbbin"))
+
+# Transform raw variables to WoE (creates new columns [original_column_name]_woe)
