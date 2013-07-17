0dee39d9397455daa138cd4e039a797496a21f0d
Tomáš Greif <tgr.cpnet>
2013-07-17 12:53:48 +0200

    Changed R-style aggregate() to sqldf, added verbose parameter.

--- R/iv.mult.r
+++ R/iv.mult.r
@@ -9,52 +9,55 @@
 #' @param summary Only total information value for variable is returned when summary is TRUE. Output is sorted by
 #' information value, starting with highest value.
 #' @param vars List of variables. If not specified, all character variables will be used
+#' @param verbose Prints additional details when TRUE. Useful mainly for debugging.
 #' @export
 #' @examples
 #' iv.mult(german_data,"gbbin")
-#' iv.mult(german_data,"gbbin",T)
-#' iv.mult(german_data,"gbbin",T,c("ca_status","housing","job","mob")) # str(german_data)
+#' iv.mult(german_data,"gbbin",TRUE)
+#' iv.mult(german_data,"gbbin",TRUE,c("ca_status","housing","job","mob")) # str(german_data)
 #' iv.mult(german_data,"gbbin",vars=c("ca_status","housing","job","mob","gb"))
-#' iv.mult(german_data,vars=c('ca_status','mob','credit_history','purpose',
-#'                            'credit_amount','savings','present_employment_since',
-#'                            'status_sex','installment_rate_income','other_debtors',
-#'                            'present_residence_since','property','age','other_installment',
-#'                            'housing','existing_credits','job','liable_maintenance_people',
-#'                            'telephone','foreign_worker'),
-#'                            "gbbin", summary=T)
+#' iv.mult(german_data,"gbbin",summary=TRUE, verbose=TRUE)
 
-
-iv.mult <- function(df,y,summary=F,vars=NULL) {
-
+iv.mult <- function(df,y,summary=FALSE,vars=NULL,verbose=FALSE) {
+  if(verbose) {
+    cat(paste("Started processing of data frame:", deparse(substitute(df)),"\n"))
+  }
+  
   if(is.null(vars)) {
     vars <- names(df)[names(df) !=y]
   }
   
   ivlist <- lapply(vars, function (x) {
       if(is.numeric(df[,x])) {
-        iv.num(df,x,y)
-      } else iv.str(df,x,y)
+        if (verbose) cat(paste("Calling iv.num for variable:", x, "\n"))
+        iv.num(df,x,y,verbose=verbose)
+      } else {
+        if (verbose) cat(paste("Calling iv.str for variable:", x, "\n"))
+        iv.str(df,x,y,verbose=verbose)  
+      }
     }
-                   )
+                  )
   
   if (summary) {
+    if (verbose) cat(paste("Preparing summary","\n"))
     ivlist <- rbind.fill(ivlist)
-    ivlist <- aggregate(x=ivlist$miv, by=list(Category=ivlist$variable), FUN=function(x) c(
-      round(sum(x),2),
-      length(x),
-      sum(ifelse(is.infinite(x),1,0))))
-    ivlist <- do.call(data.frame, ivlist)
-    ivlist <- ivlist[with(ivlist,order(-x.1)), ]
-    
-    ivlist$Strength[ivlist$x.1 >= 1] <- 1
-    ivlist$Strength[ivlist$x.1 >= .5 & ivlist$x.1 < 1] <- 2
-    ivlist$Strength[ivlist$x.1 >= .2 & ivlist$x.1 < .5] <- 3
-    ivlist$Strength[ivlist$x.1 >= .1 & ivlist$x.1 < .2] <- 4
-    ivlist$Strength[ivlist$x.1 >= .02 & ivlist$x.1 < .1] <- 5
-    ivlist$Strength[ivlist$x.1 < .02] <- 6
+    ivlist <- sqldf("select 
+                        variable as Variable,
+                        sum(miv) as InformationValue, 
+                        count(*) as Bins,
+                        sum(case when outcome_0 = 0 or outcome_1 = 0 then 1 else 0 end) as ZeroBins
+                     from ivlist 
+                     group by variable 
+                     order by InformationValue desc") 
+
+    ivlist$Strength[ivlist$InformationValue >= 1] <- 1
+    ivlist$Strength[ivlist$InformationValue >= .5 & ivlist$InformationValue < 1] <- 2
+    ivlist$Strength[ivlist$InformationValue >= .2 & ivlist$InformationValue < .5] <- 3
+    ivlist$Strength[ivlist$InformationValue >= .1 & ivlist$InformationValue < .2] <- 4
+    ivlist$Strength[ivlist$InformationValue >= .02 & ivlist$InformationValue < .1] <- 5
+    ivlist$Strength[ivlist$InformationValue < .02] <- 6
     ivlist$Strength <- factor(ivlist$Strength, levels=c(1,2,3,4,5,6), 
                               labels= c("Suspicious","Very strong","Strong","Average","Weak","Wery weak"))
-    names(ivlist) <- c("Variable","InformationValue","Bins","Zero Bins","Strength")
   }
   ivlist
 }
