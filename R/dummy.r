6fc49ca8214afb2cc30f030ca912809667a3511d
Tomáš Greif <tgr.cpnet>
2013-08-06 13:19:57 +0200

    added dummy function

---
+++ R/dummy.r
@@ -0,0 +1,12 @@
+#' Příšerně žluťoučký kůň úpěl ďábelské ódy.
+#'
+#' Příšerně žluťoučký kůň úpěl ďábelské ódy.Příšerně žluťoučký kůň úpěl ďábelské ódy.
+#'
+#' @param x Příšerně žluťoučký kůň úpěl ďábelské ódy.Příšerně žluťoučký kůň úpěl ďábelské ódy.
+#' @export
+#' @examples
+#' dummy(x="Příšerně žluťoučký kůň úpěl ďábelské ódy.Příšerně žluťoučký kůň úpěl ďábelské ódy.")
+
+dummy <- function(x) {
+  print("dummy")
+}
